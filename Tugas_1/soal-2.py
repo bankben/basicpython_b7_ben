0ff5236462436987b6d60cebb6fff4f3555ef3ed
bankben <85384989+bankben@users.noreply.github.com>
2021-06-11 23:50:29 +0700

    Add files via upload

---
+++ Tugas_1/soal-2.py
@@ -0,0 +1,9 @@
+#Program Menghitung Luask Lingkaran
+#oleh Beni Oktopiansah
+
+jari_jari = int(input ("Masukkan jari-jari lingakaran dalam cm :"))
+luas = float(22)/float(7)* (jari_jari**(int(2)))
+
+print("Luas lingkaran dengan jari-jari : %i cm adalah %.2f cm\u00b2" % (jari_jari, luas))
+
+#Terima kasih
